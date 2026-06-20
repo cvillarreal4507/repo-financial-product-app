e9b60f8c2f92646030cd15747843970fde119318
Cristopher Villarreal <villarrealalvac@gmail.com>
2026-06-20 18:55:27 -0500

    feat: Componetize input's

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() placeholder = '';
+  @Input() type = 'text';
+  @Input() id = '';
+  @Input() required = false;
+  @Input() disabled = false;
+  @Input() errorMessage = '';
+  @Input() isInvalid = false;
+  @Input() readonly = false;
+
+  value: any = '';
+
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onInput(event: Event): void {
+    const val = (event.target as HTMLInputElement).value;
+    this.value = val;
+    this.onChange(val);
+  }
+
+  onBlur(): void {
+    this.onTouch();
+  }
+}
