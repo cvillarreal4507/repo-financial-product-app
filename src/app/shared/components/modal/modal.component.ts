f91f3a0cf630cdb949bcde7191940ee9c5e762de
Cristopher Villarreal <villarrealalvac@gmail.com>
2026-06-20 18:58:37 -0500

    feat: Componetize Modal's

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent {
+  @Input() isOpen = false;
+  @Input() title = 'Confirmación';
+  @Output() confirm = new EventEmitter<void>();
+  @Output() cancel = new EventEmitter<void>();
+
+  onConfirm(): void {
+    this.confirm.emit();
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+  }
+}
